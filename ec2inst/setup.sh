edfecea17cf1492c04d1956f851ce1f474b2d735
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-11-04 03:14:30 +0000

    Added config and software dev repo clones.
    Clone basic repos for dev and some repos currently working on.

--- ec2inst/setup.sh
+++ ec2inst/setup.sh
@@ -2,3 +2,13 @@
 
 # This installs tools for doing software development.
 sudo yum groupinstall "Development Tools"
+
+# Basic stuff
+git clone https://github.com/jdellithorpe/config.git
+git clone https://github.com/jdellithorpe/scripts.git
+git clone https://github.com/jdellithorpe/utils.git
+
+# Things I'm working on 
+git clone https://github.com/jdellithorpe/ldbc-snb-datagen-cloudlab-profile.git
+git clone https://github.com/ldbc/ldbc_snb_driver.git
+git clone https://github.com/jdellithorpe/ramcloud-cloudlab-profile-m510.git
