5a8a95bb36da5dbbc11673805ac3704e0fb84955
Jonathan Ellithorpe <jde@cs.stanford.edu>
2017-11-15 13:20:58 -0700

    Added vimf bash function.
    Handy bash function that is shorthand for
    
    vim $(find . -name $1)

--- cloudlab/setup.sh
+++ cloudlab/setup.sh
@@ -16,4 +16,13 @@ echo -n "  Installing tmux configuration... "
 cp .tmux.conf ~/
 echo "SUCCESS"
 
+echo -n "  Installing bash configuration... "
+cat >> ~/.bashrc <<EOM
+
+vimf () {
+  vim \$(find . -name \$1)
+}
+EOM
+echo "SUCCESS"
+
 echo "Setup finished successfully!"
