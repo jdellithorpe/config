2ae7702230b01197817066595e212c8411d351f9
Jonathan Ellithorpe <jonathan@ours-tech.com>
2018-04-26 10:33:31 -0700

    Updated ours-gateway config.

--- ours-gateway/setup.sh
+++ ours-gateway/setup.sh
@@ -2,4 +2,31 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 
-../global/setup.sh
+echo "Running setup..."
+
+echo -n "  Installing git configuration... "
+ln -f -s $SCRIPTPATH/.gitconfig ~/
+echo "SUCCESS"
+
+echo -n "  Installing vim configuration... "
+ln -f -s $SCRIPTPATH/../global/.vimrc ~/
+ln -f -s $SCRIPTPATH/../global/.vim ~/
+echo "SUCCESS"
+
+echo -n "  Installing tmux configuration... "
+ln -f -s $SCRIPTPATH/../global/.tmux.conf ~/
+echo "SUCCESS"
+
+echo -n "  Installing bash configuration... "
+cat >> ~/.bashrc <<EOM
+
+source $SCRIPTPATH/../global/.bashrc
+
+if [[ "\$(hostname --short)" == "ours-gateway" ]] && [[ -z "\$TMUX" ]] && [[ "\$SSH_CONNECTION" != "" ]]
+then
+  tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
+fi
+EOM
+echo "SUCCESS"
+
+echo "Setup finished successfully!"
