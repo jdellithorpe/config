94b5d5bffa9849afb43af83d2a84e37375d78c34
Jonathan Ellithorpe <jde@cs.stanford.edu>
2019-08-24 19:19:35 +0000

    Added software install for ubuntu16.04

--- ubuntu16.04/setup.sh
+++ ubuntu16.04/setup.sh
@@ -2,4 +2,5 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 
+sudo ./software-install.sh
 ../global/setup.sh
--- ubuntu16.04/software-install.sh
+++ ubuntu16.04/software-install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
+echo -e "\n===== INSTALLING COMMON UTILITIES ====="
 apt-get update
-apt-get --assume-yes install tree maven cscope ctags htop gnuplot clang cmake
+apt-get --assume-yes install vim tmux pdsh tree axel htop ctags cscope cmake clang gnuplot sysstat pcp
+# Maven
+echo -e "\n===== INSTALLING MAVEN ====="
+apt-get --assume-yes install maven
+# Python packages
+echo -e "\n===== INSTALLING PYTHON PACKAGES ====="
+apt-get --assume-yes install python-numpy python-pandas python-matplotlib
